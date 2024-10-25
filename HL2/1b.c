cbd34e7f0a9d4fe3f634494381fc2482692b5e3b
Shane Gomes <48016201+shaneg07@users.noreply.github.com>
2024-10-25 14:49:15 +0530

    Add files via upload

---
+++ HL2/1b.c
@@ -0,0 +1,60 @@
+/*
+============================================================================
+Name : 1b.c
+Author : Shane Gomes
+Roll no : MT2024139
+Description : Write a separate program (for each time domain) to set a interval timer in 10sec and
+                10micro second
+                b. ITIMER_VIRTUAL
+============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>   // For signal handling
+#include <sys/time.h> // For timers
+
+// Signal handler for SIGVTALRM
+void handle_virtual_alarm(int signum) {
+    printf("Timer expired (ITIMER_VIRTUAL)\n");
+}
+
+int main() {
+    struct itimerval timer; 
+
+    
+    signal(SIGVTALRM, handle_virtual_alarm);
+
+    
+    timer.it_value.tv_sec = 10;   
+    timer.it_value.tv_usec = 10;  
+    timer.it_interval.tv_sec = 10; 
+    timer.it_interval.tv_usec = 10;
+
+
+    if (setitimer(ITIMER_VIRTUAL, &timer, NULL) == -1) {
+        perror("Error setting ITIMER_VIRTUAL");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        while(1){
+            int i = 0;
+            i++;
+        }
+    }
+
+    return 0;
+}
+
+/*
+============================================================================
+Command line: cc 1b.c && ./a.out
+Output: 
+Timer expired (ITIMER_VIRTUAL)
+Timer expired (ITIMER_VIRTUAL)
+...
+(every 10 sec 10 usec)
+============================================================================
+*/
